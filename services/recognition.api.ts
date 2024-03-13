a33db0ccdd9c17c8d5a5746c317f31e31eeb611b
Steven theodore <steven@theacemeta.com>
2024-03-13 22:42:48 +0700

    Fix types

--- services/recognition.api.ts
+++ services/recognition.api.ts
@@ -1,12 +1,12 @@
 import { StrapiResponse } from '@/domain/types/common.types'
-import { Recognition } from '@/domain/types/recognition.types'
+import { TRecognition } from '@/domain/types/recognition.types'
 import { apiInstance } from './api'
 
 const BASE_URL = '/recognitions'
 
 export const RecognitionApi = {
-  getAll: async function (): Promise<StrapiResponse<Recognition>> {
-    const result = await apiInstance.get<StrapiResponse<Recognition>>(
+  getAll: async function (): Promise<StrapiResponse<TRecognition>> {
+    const result = await apiInstance.get<StrapiResponse<TRecognition>>(
       `${BASE_URL}?populate=*`,
     )
 
